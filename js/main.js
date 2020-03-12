2e5768e6b4ccc801d2c62a0fb12e68d5e042eb00
Vladimir <Velogon6565@gmail.com>
2020-03-12 14:49:02 +0200

    Домашнее задание номер 7

---
+++ js/main.js
@@ -0,0 +1,50 @@
+"Use strict"
+
+let buttonStart = document.getElementById("start"),
+    budgetValue = document.querySelectorAll(".budget-value"),
+    daybudget = document.querySelectorAll(".daybudget-value"),
+    levelValue = document.querySelectorAll(".level-value"),
+    expensesValue = document.querySelectorAll(".expenses-value"),
+    optionalexpensesValue = document.querySelectorAll(".optionalexpenses-value"),
+    incomeValue = document.querySelectorAll(".income-value"),
+    monthsavingsValue = document.querySelectorAll(".monthsavings-value"),
+    yearsavingsValue = document.querySelectorAll(".yearsavings-value"),
+    yearValue = document.querySelectorAll(".year-value"),
+    monthValue = document.querySelectorAll(".month-value"),
+    dayValue = document.querySelectorAll(".day-value"),
+    chooseExpenses = document.querySelectorAll(".expenses-item"),
+    buttons = document.getElementsByTagName("button"),
+    accept1 = document.querySelector(".count-budget-btn"),
+    accept2 = document.querySelector(".optionalexpenses-btn"),
+    optionalExpensesItem = document.querySelectorAll(".optionalexpenses"),
+    chooseIncome = document.querySelector("#income"),
+    checkbox = document.querySelector("#savings"),
+    sum = document.querySelector("#sum"),
+    year = document.querySelector(".year-value"), 
+    month = document.querySelector(".month-value"),
+    day = document.querySelector(".day-value");
+    
+
+
+console.log(day);
+console.log(month);
+console.log(year);
+console.log(sum);
+console.log(checkbox);
+console.log(chooseIncome);
+console.log(optionalExpensesItem);
+console.log(accept1);
+console.log(accept2);
+console.log(chooseExpenses);
+console.log(buttonStart);
+console.log(budgetValue);
+console.log(daybudget);
+console.log(levelValue);
+console.log(expensesValue);
+console.log(optionalexpensesValue);
+console.log(incomeValue);
+console.log(monthsavingsValue);
+console.log(yearsavingsValue);
+console.log(yearValue);
+console.log(monthValue);
+console.log(dayValue);
